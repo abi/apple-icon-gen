8efe1c063a80fcb8af057c2cf572fa5bbf8d1d42
Abi Raja <abimanyuraja@gmail.com>
2017-04-09 10:36:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+## Introduction
+
+![alt text](screenshot.png "Screenshot of Xcode appicon")
+
+
+Transform PNG files into the requisite sizes for Apple platforms. Doesn't require ImageMagick which can be a pain to get working on a Mac. Instead, it uses the `sharp` module.
+
+## Arguments
+
+`--input input file`   
+
+`--output output directory`   
+
+`--spec list of images sizes to generate, see example below`
+
+## Example usage
+
+```
+npm install -g apple-icon-gen
+
+apple-icon-gen ---input=data/input/icon.png --output=data/output --spec=20@2 20@3 29@2 29@3 40@2 40@3 60@2 60@3
+```
+
+## Troubleshooting
+
+The code is really simple. Modify/debug it if there are exceptions.
+
---
+++ index.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+var sharp = require('sharp');
+var argv = require('minimist')(process.argv.slice(2));
+
+try {
+    var inputFile = path.normalize(argv.input);
+    var inputFileBasename = path.basename(inputFile, '.png');
+} catch (e) {
+    console.error('Check your input path');
+    process.exit(1);
+}
+
+var outputSizes = argv.spec;
+var outputSizesSplit = outputSizes.split(' ');
+
+var outputFiles = [];
+
+for (var i = 0; i < outputSizesSplit.length; i++) {
+    var imageSpec = outputSizesSplit[i];
+    var size = parseInt(imageSpec.split('@')[0]);
+    var mag = parseInt(imageSpec.split('@')[1]);
+
+    outputFiles.push(
+        {
+            name: inputFileBasename + '-' + imageSpec + 'x.png',
+            size: size * mag
+        }
+    );
+}
+
+for (var i = 0; i < outputFiles.length; i++) {
+    var outputFile = outputFiles[i];
+    sharp(path.resolve(inputFile))
+        .resize(outputFile.size)
+        .toFile(path.join(argv.output, outputFile.name), function (err, info) {
+            console.log(err, info)
+            
+        });
+}
